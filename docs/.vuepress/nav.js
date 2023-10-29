3c3462aa1958ea280b143195b854b93adb808b14
BlazeMaple <193237147@qq.com>
2023-10-29 17:29:56 +0800

    deploy

---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,51 @@
+module.exports = [
+    {text: '首页', link: '/'},
+    {
+        text: 'Java基础', link: '/pages/2da1db/', items: [
+            {
+                text: '基础知识', items: [
+                    {text: 'Java的基本数据类型', link: '/pages/2da1db/'},
+                ]
+            },
+            {
+                text: '集合框架', items: [
+                    {text: 'Java集合核心知识总结', link: ' /pages/9f1e69/'},
+                ]
+            },
+        ]
+    },
+    {
+        text: 'Java进阶', link: '/pages/9bff74/', items: [
+            {
+                text: 'IO流', items: [
+                    {text: 'Java基础IO总结', link: '/pages/9bff74/'},
+                ]
+            },
+            {
+                text: '并发编程', items: [
+                    {text: '并发编程基础总结', link: ' /pages/faa5b5/'},
+                ]
+            },
+            {
+                text: 'JVM', items: [
+                    {text: 'JVM基础总结', link: ' /pages/1f79d5/'},
+                ]
+            },
+        ]
+    },
+    {
+        text: '数据库', link: '/pages/5bdae6/', items: [
+            {
+                text: 'MySQL', items: [
+                    {text: 'MySQL核心知识小结', link: '/pages/5bdae6/'},
+                ]
+            },
+            {
+                text: 'Redis', items: [
+                    {text: 'Redis核心入门知识简记', link: ' /pages/b422b9/'},
+                ]
+            },
+        ]
+    },
+
+]
---
+++ docs/index.md
@@ -0,0 +1,27 @@
+---
+home: true
+heroImage: /img/gif/logo.gif
+heroText: BlazeMaple
+tagline: 🚀 等风来，不如追风去！
+actionText: 开始使用 →
+actionLink: /pages/2da1db/
+bannerBg: none # auto => 网格纹背景(有bodyBgImg时无背景)，默认 | none => 无 | '大图地址' | background: 自定义背景样式       提示：如发现文本颜色不适应你的背景时可以到palette.styl修改$bannerTextColor变量
+
+features: # 可选的
+- title: 学习
+  details: 学习是一个无止境的旅程，没有终点。 – 坦托·安东尼奥·帕罗蒂（Tanto Antonio Parodi）
+- title: 实践
+  details: 言之难尽，而试之易知也。 ——傅玄
+- title: 输出
+  details: 我坚信，自我教育是唯一的教育形式。 ——艾萨克·阿西莫夫(Isaac Asimov)
+
+# 文章列表显示方式: detailed 默认，显示详细版文章列表（包括作者、分类、标签、摘要、分页等）| simple => 显示简约版文章列表（仅标题和日期）| none 不显示文章列表
+postList: none
+---
+
+
+
+
+
+
+
