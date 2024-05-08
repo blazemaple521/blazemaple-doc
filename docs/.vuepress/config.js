2474ed497e224b03ddc486911c3bbdb7dfc2d147
sqp <193237147@qq.com>
2024-05-08 17:23:56 +0800

    提交MySQL45讲-实践篇1内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -31,8 +31,8 @@ module.exports = {
 
         // sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
         updateBar: { // 最近更新栏
-            showToArticle: false, // 显示到文章页底部，默认true
-            // moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
+            showToArticle: true, // 显示到文章页底部，默认true
+            moreArticle: '/pages/2da1db/' // “更多文章”跳转的页面，默认'/archives'
         },
         // titleBadge: false, // 文章标题前的图标是否显示，默认true
         // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
@@ -80,33 +80,33 @@ module.exports = {
         }],
         ['fulltext-search'], // 全文搜索
         // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
-        // [
-        //     'thirdparty-search',
-        //     {
-        //         thirdparty: [
-        //             {
-        //                 title: '在MDN中搜索',
-        //                 frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-        //                 behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
-        //             },
-        //             {
-        //                 title: '在Runoob中搜索',
-        //                 frontUrl: 'https://www.runoob.com/?s=',
-        //             },
-        //             {
-        //                 title: '在Vue API中搜索',
-        //                 frontUrl: 'https://cn.vuejs.org/v2/api/#',
-        //             },
-        //             {
-        //                 title: '在Bing中搜索',
-        //                 frontUrl: 'https://cn.bing.com/search?q=',
-        //             },
-        //             {
-        //                 title: '通过百度搜索本站的',
-        //                 frontUrl: `https://www.baidu.com/s?wd=`,
-        //             },
-        //         ],
-        //     }],
+        [
+            'thirdparty-search',
+            {
+                thirdparty: [
+                    // {
+                    //     title: '在MDN中搜索',
+                    //     frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
+                    //     behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+                    // },
+                    // {
+                    //     title: '在Runoob中搜索',
+                    //     frontUrl: 'https://www.runoob.com/?s=',
+                    // },
+                    // {
+                    //     title: '在Vue API中搜索',
+                    //     frontUrl: 'https://cn.vuejs.org/v2/api/#',
+                    // },
+                    // {
+                    //     title: '在Bing中搜索',
+                    //     frontUrl: 'https://cn.bing.com/search?q=',
+                    // },
+                    {
+                        title: '通过百度搜索本站的',
+                        frontUrl: `https://www.baidu.com/s?wd=`,
+                    },
+                ],
+            }],
         ['one-click-copy', { // 代码块复制按钮
             copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
             copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
